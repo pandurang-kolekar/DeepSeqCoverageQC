2b550e22a7e3b80e17d1e10e95eeb6a34e7a9e0b
pandurang-kolekar <pandurang.kolekar@gmail.com>
2023-06-24 11:56:42 -0500

    Added tests

---
+++ tests/test_panelIndexer.py
@@ -0,0 +1,9 @@
+import panelqc.panelIndexer
+
+
+def test_expandRegion():
+    assert panelqc.panelIndexer.expandRegion(10, 10, 0) == [10]
+
+
+def test_expandRegionWithPadding():
+    assert panelqc.panelIndexer.expandRegion(10, 10, 1) == [9, 10, 11]
